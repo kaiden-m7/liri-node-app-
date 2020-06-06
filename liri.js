9a6c57ddc126aa2dfc8cbaee3cba0ac7905f1da4
Kaiden Munsey <kaidenmnsey@aol.com>
2020-06-06 12:01:02 -0400

    adding global variables and installing npm's

--- liri.js
+++ liri.js
@@ -1,2 +1,10 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+let keys = require("./keys.js");
+let axios = require("axios");
+let moment = require("moment");
+let Spotify = require("node-spotify-api");
+let fs = require("fs");
+let spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret,
+});
